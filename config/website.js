d230dc2231a752ce3409e671cf71eb1387d6b58b
Alex Welcing <alexwelcing@gmail.com>
2019-06-20 10:37:19 -0400

    update copy

--- config/website.js
+++ config/website.js
@@ -1,20 +1,20 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-  siteTitle: 'Emma', // Navigation and Site Title
-  siteTitleAlt: 'Emma - Gatsby Starter Portfolio', // Alternative Site title for SEO
-  siteHeadline: 'Creating marvelous art & blazginly fast websites', // Headline for schema.org JSONLD
-  siteTitleShort: 'Emma', // short_name for manifest
-  siteUrl: 'https://emma.lekoarts.de', // Domain of your site. No trailing slash!
+  siteTitle: 'Travels', // Navigation and Site Title
+  siteTitleAlt: 'Quick reference site for our photos and favorites.', // Alternative Site title for SEO
+  siteHeadline: 'Alex and Lauren honeymoon notes, June 2019.', // Headline for schema.org JSONLD
+  siteTitleShort: 'Travels', // short_name for manifest
+  siteUrl: 'https://laurlex.ventures', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logo.png', // Used for SEO and manifest
-  siteDescription: 'Minimalistic bright portfolio with full-width grid and large images',
-  author: 'LekoArts', // Author for schema.org JSONLD
+  siteDescription: 'Images and links to places we loved.',
+  author: 'AlexWelcing', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@emma', // Twitter Username
-  ogSiteName: 'emma', // Facebook Site Name
+  userTwitter: '@alexwelcing', // Twitter Username
+  ogSiteName: 'alexwelcing', // Facebook Site Name
   ogLanguage: 'en_US',
-  googleAnalyticsID: 'UA-47519312-3',
+    googleAnalyticsID: 'UA-97698165-1',
 
   // Manifest and Progress color
   themeColor: '#3498DB',
